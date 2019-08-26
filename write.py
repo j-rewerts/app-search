47ae9cd85afb087ce93dfb2f3280c131abd88774
j-rewerts <jaredrewerts@gmail.com>
2019-08-26 10:39:59 -0600

    Added performance monitoring to syncronous writes.

--- write.py
+++ write.py
@@ -1,16 +1,26 @@
 from swiftype_app_search import Client
+import time
+
+FILE = 'council_sample.csv'
 
 print('Setting up connection')
 client = Client(
     api_key='',
-    base_endpoint='162.106.81.175:3002/api/as/v1',
+    base_endpoint='192.168.1.1:3002/api/as/v1',
     use_https=False,
 )
 print('connection established')
 
-with open('council_reports.csv', 'r') as report:
-    for line in report:
-        print('Indexing document')
-        client.index_document('single-line-doc', {
-            'contents': line
-        })
+def main():
+    with open(FILE, 'r') as report:
+        for line in report:
+            print('Indexing document')
+            client.index_document('single-line-doc', {
+                'contents': line
+            })
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+    main()
+    elapsed = time.perf_counter() - start
+    print(f"Program completed in {elapsed:0.5f} seconds.")
